376cbeeb48bfcaac27fd3f942b8a19c240135434
Zac Willmington <zac.willmington@gmail.com>
2022-01-30 16:32:31 -0600

    Deploy tokens to in test file

--- test/dex.js
+++ test/dex.js
@@ -2,9 +2,15 @@ const Dai = artifacts.require('../contracts/mocks/Dai.sol');
 const Bat = artifacts.require('../contracts/mocks/Bat.sol');
 const Zrx = artifacts.require('../contracts/mocks/Zrx.sol');
 const Rep = artifacts.require('../contracts/mocks/Rep.sol');
+const Dex = artifacts.require('../contracts/Dex.sol');
 
 contract('Dex', () => {
     let dai, bat, zrx, rep;
+
+    const tickers = ['DAI', 'BAT', 'ZRX', 'REP'];
+    const [DAI, BAT, ZRX, REP] = tickers.map(ticker => {
+        return web3.utils.fromAscii(ticker);
+    });
 
     beforeEach(async () => {
         ([dai, bat, zrx, rep] = await Promise.all([
@@ -13,5 +19,13 @@ contract('Dex', () => {
             Zrx.new(),
             Rep.new()
         ]));
+        
+        const dex = await Dex.new();
+        await Promise.all([
+            dex.addToken(DAI, dai.address),
+            dex.addToken(BAT, bat.address),
+            dex.addToken(ZRX, zrx.address),
+            dex.addToken(REP, rep.address)
+        ]);
     });
 })
